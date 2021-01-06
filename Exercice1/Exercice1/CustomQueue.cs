b71c14be68a63ddb73bad9ff04567d9084926c95
quentinjacq <quentin.jacq@gmail.com>
2021-01-06 11:53:21 +0100

    exercice1finito

--- Exercice1/Exercice1/CustomQueue.cs
+++ Exercice1/Exercice1/CustomQueue.cs
@@ -13,7 +13,7 @@ namespace Exercice1
 
         Node<T> head { get; set; }
 
-        public void addNode(Node<T> node, bool onHead = false)
+        public void Enqueue(Node<T> node)
         {
             Node<T> new_node = new Node<T>(node.data);
 
@@ -21,65 +21,68 @@ namespace Exercice1
                 head = new_node;
             else
             {
-                if (onHead == true)
-                {
-                    Node<T> temp = head;
-                    new_node.next = temp;
-                    temp.previous = new_node;
-                    head = new_node;
-                }
-                else
-                {
-                    Node<T> current = head;
-                    while (current.next != null)
-                    {
-                        current = current.next;
-                    }
-                    current.next = new_node;
-                    new_node.previous = current;
-                }
+                Node<T> temp = head;
+                new_node.next = temp;
+                temp.previous = new_node;
+                head = new_node;
             }
         }
 
-
-        public Node<T> getNode(int index)
+        public Node<T> DequeueAndRetreive()
         {
-            int pos = 0;
             Node<T> current = head;
-            while (index > pos)
+            Node<T> dequeued;
+            while (current.next != null)
             {
-                pos++;
+                dequeued = current.previous;
                 current = current.next;
             }
+            current.previous.next = null;
+            current.previous = null;
             return current;
         }
 
-        public void setNode(Node<T> node, int index)
-        {
-            int pos = 0;
-            Node<T> current = head;
-            Node<T> new_node = new Node<T>(node.data);
-            while (pos < index)
-            {
-                pos++;
-                current = current.next;
-            }
-            current.previous.next = new_node;
-            new_node.previous = current.previous;
-            new_node.next = current.next;
-            if (current.next != null)
-                current.next.previous = new_node;
-        }
+        //public Node<T> getNode(int index)
+        //{
+        //    int pos = 0;
+        //    Node<T> current = head;
+        //    while (index > pos)
+        //    {
+        //        pos++;
+        //        current = current.next;
+        //    }
+        //    return current;
+        //}
+
+        //public void setNode(Node<T> node, int index)
+        //{
+        //    int pos = 0;
+        //    Node<T> current = head;
+        //    Node<T> new_node = new Node<T>(node.data);
+        //    while (pos < index)
+        //    {
+        //        pos++;
+        //        current = current.next;
+        //    }
+        //    current.previous.next = new_node;
+        //    new_node.previous = current.previous;
+        //    new_node.next = current.next;
+        //    if (current.next != null)
+        //        current.next.previous = new_node;
+        //}
+
 
         public void print()
         {
-            string s = "";
+            string s = "[";
             Node<T> current = head;
             while (current != null)
             {
-                s += current.data + " ";
+                s += current.data + " ; ";
                 current = current.next;
             }
+            s = s.Remove(s.Length - 3);
+            s += "]";
             Console.WriteLine(s);
         }
     }
--- Exercice1/Exercice1/Program.cs
+++ Exercice1/Exercice1/Program.cs
@@ -6,16 +6,59 @@ namespace Exercice1
     {
         static void Main(string[] args)
         {
-            CustomQueue<string> l = new CustomQueue<string>(new Node<string>("Coucou"));
-            l.addNode(new Node<string>("Bonjour"));
-            l.addNode(new Node<string>("Salut"));
-            Console.WriteLine(l.getNode(1).data);
-            Console.WriteLine(l.getNode(2).data);
-            l.setNode(new Node<string>("Heyy"), 2);
-            Console.WriteLine(l.getNode(2).data);
-            l.print();
+            Console.WriteLine("Hello, and welcome in our Custom Queue.\n");
+
+            Console.WriteLine("PRESS ENTER TO CONTINUE...");
             Console.ReadKey();
 
+            Console.WriteLine("\nIn a first part, we simply enqueue 5 numbers : ");
+            Console.WriteLine("     - 14");
+            Console.WriteLine("     - 18");
+            Console.WriteLine("     - 39");
+            Console.WriteLine("     - 45");
+            Console.WriteLine("     - 303\n");
+
+            CustomQueue<int> l = new CustomQueue<int>(new Node<int>(14));
+            l.Enqueue(new Node<int>(18));
+            l.Enqueue(new Node<int>(39));
+            l.Enqueue(new Node<int>(45));
+            l.Enqueue(new Node<int>(303));
+
+            Console.WriteLine("Then we dequeue 2 times.\n");
+            Node<int> temp = l.DequeueAndRetreive(); //We created a temporary variable because it is asked to retreive the value when we deuqueue it
+            temp = l.DequeueAndRetreive();
+
+            Console.WriteLine("Here is how our final queue looks like : ");
+            l.print();
+
+            Console.WriteLine("\nPRESS ENTER TO CONTINUE...");
+            Console.ReadKey();
+
+            Console.WriteLine("\nIn a second part, we enqueue 10 strings : "); //We make a second example with another variable type to proove the generics works
+            Console.WriteLine("     - It");
+            Console.WriteLine("     - Is");
+            Console.WriteLine("     - Raining");
+            Console.WriteLine("     - Cats");
+            Console.WriteLine("     - And");
+            Console.WriteLine("     - Dogs\n");
+
+            CustomQueue<string> l2 = new CustomQueue<string>(new Node<string>("It"));
+            l2.Enqueue(new Node<string>("Is"));
+            l2.Enqueue(new Node<string>("Raining"));
+            l2.Enqueue(new Node<string>("Cats"));
+            l2.Enqueue(new Node<string>("And"));
+            l2.Enqueue(new Node<string>("Dogs"));
+
+            Console.WriteLine("Then we dequeue 3 times.\n");
+            Node<string> temp2 = l2.DequeueAndRetreive();
+            temp2 = l2.DequeueAndRetreive();
+            temp2 = l2.DequeueAndRetreive();
+
+            Console.WriteLine("Here is how our final queue looks like : ");
+            l2.print();
+
+            Console.WriteLine("\nPRESS ENTER TO QUIT...");
+            Console.ReadKey();
         }
     }
 }
