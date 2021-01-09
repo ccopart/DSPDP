398c45da1280cc8596394be262d267289c1f164e
Charles COPART <charles.copart@hotmail.fr>
2021-01-09 22:28:27 +0100

    create class Player

---
+++ Exercice3/Exercice3/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice3
+{
+    class Player
+    {
+        string pseudo;
+        int position = 0;
+        bool jail = false;
+
+        public string GetPseudo()
+        {
+            return pseudo;
+        }
+        public void SetPseudo(string pseudo)
+        {
+            this.pseudo = pseudo;
+        }
+        public int GetPosition()
+        {
+            return position;
+        }
+        public void SetPosition(int position)
+        {
+
+            this.position = position;
+        }
+        public bool GetJail()
+        {
+            return jail;
+        }
+        public void SetJail(bool jail)
+        {
+            this.jail = jail;
+        }
+    }
+}
