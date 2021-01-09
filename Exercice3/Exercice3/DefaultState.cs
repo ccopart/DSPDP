f322672965928be08d9c36480a78c24e786f85c0
quentinjacq <quentin.jacq@gmail.com>
2021-01-09 23:52:30 +0100

    defaultstateok

--- Exercice3/Exercice3/DefaultState.cs
+++ Exercice3/Exercice3/DefaultState.cs
@@ -8,15 +8,30 @@ namespace Exercice3
     {
         public State DoState(Player player)
         {
-            bool continuePlaying = true;
-            while (continuePlaying)
+            int numberOfConsecutiveDoubles = 0;
+            while(true)
             {
                 var dices = player.RollTheDices();
+                if (numberOfConsecutiveDoubles == 2 && dices.Item3)//if it is the 3 consecutive double
+                {
+                    player.SetPosition(10);
+                    return player.jailState;
+                }
                 player.IncrementPosition(dices);
+                if (player.GetPosition() == 30)
+                {
+                    player.SetPosition(10);
+                    return player.jailState;
+                }
+                else
+                {
+                    if (dices.Item3)//If the dices are equals
+                    {
+                        numberOfConsecutiveDoubles++;
+                    }
+                    else return player.defaultState;
+                }
             }
-           
-
-            return player.defaultState;
         }
     }
 }
