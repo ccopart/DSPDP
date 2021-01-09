95647c0d5074b6d6311d7d956bae51c046169b39
Charles COPART <charles.copart@hotmail.fr>
2021-01-10 00:02:13 +0100

    fix(JailState): add messages

--- Exercice3/Exercice3/JailState.cs
+++ Exercice3/Exercice3/JailState.cs
@@ -12,7 +12,16 @@ namespace Exercice3
             player.jailPlayCounts += 1;
             if(play.Item3 || player.jailPlayCounts == 3)
             {
+                if(play.Item3 && player.jailPlayCounts != 3)
+                {
+                    Console.WriteLine("Joueur " + player.GetPseudo() + "a fait un double, il sort de prison !");
+                }
+                else
+                {
+                    Console.WriteLine("Joueur " + player.GetPseudo() + "a passé 3 tours en prison, il est libre !");
+                }
                 player.IncrementPosition(play);
+                Console.WriteLine("Joueur " + player.GetPseudo() + " avance jusqu'à la case " + player.GetPosition()); 
                 player.jailPlayCounts = 0;
                 return player.defaultState;
             }
