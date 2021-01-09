b003a3e148a7d2bbb766a88c1a3449a8c16ece4c
Charles COPART <charles.copart@hotmail.fr>
2021-01-09 19:28:19 +0100

    exo 2 fini inchallah

--- Exercice2/Exercice2/Program.cs
+++ Exercice2/Exercice2/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+
 
 namespace Exercice2
 {
     class Program
     {
+        
+        static string [] Splitting () 
+        {
+            string [] lines = System.IO.File.ReadAllLines("text.txt", System.Text.Encoding.GetEncoding("iso-8859-1"));
+            return lines;
+        }
+
+        public Dictionary<string, int> Map(string line)
+        {
+            Dictionary<string, int> nombreOccurenceMots = new Dictionary<string, int>();
+            var regex = new Regex(@"\b[\s,.-:;']*");
+            var words = regex.Split(line).Where(x => !string.IsNullOrEmpty(x));
+            foreach (string word in words)
+            {
+                if (nombreOccurenceMots.ContainsKey(word)) nombreOccurenceMots[word] = +1;
+                else nombreOccurenceMots.Add(word, 1);
+            }
+            return nombreOccurenceMots;
+        }
+
+        public Dictionary<string, int> Reduce(Dictionary<string, int> dicoParLigne, Dictionary<string, int> nombreTotalMots)
+        {
+            foreach (var i in dicoParLigne)
+            {
+                if (nombreTotalMots.ContainsKey(i.Key)) nombreTotalMots[i.Key] = +1;
+                else nombreTotalMots.Add(i.Key, 1);
+            }
+            return nombreTotalMots;
+        }
+
+        public Dictionary<string, int> GetWords()
+        {
+            var result = new Dictionary<string, int>();
+            string[] lines = Splitting();
+            Parallel.ForEach(lines, line => {
+                Dictionary<string, int> lineDict = Map(line);
+                lock(result)
+                {
+                    Reduce(lineDict, result);
+                }
+            }
+            );
+            result.OrderByDescending(kv => kv.Value).Take((int)TopCount).ToDictionary(kv => kv.Key, kv => kv.Value);
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] test = Splitting();
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
