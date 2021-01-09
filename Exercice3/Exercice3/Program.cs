de694276e8971f8c8eb21fc8e89539ce20b01cd5
quentinjacq <quentin.jacq@gmail.com>
2021-01-09 22:25:31 +0100

    exo3rolldices

--- Exercice3/Exercice3/Program.cs
+++ Exercice3/Exercice3/Program.cs
@@ -7,6 +7,19 @@ namespace Exercice3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var dices = RollTheDices();
+            Console.WriteLine(dices.Item1);
+            Console.WriteLine(dices.Item2);
+        }
+
+        public static (int, int) RollTheDices()
+        {
+            Random rnd = new Random();
+            int dice1 = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
+
+            return (dice1, dice2);
         }
     }
 }
