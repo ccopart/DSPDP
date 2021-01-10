9e41c990272b4c3ea3e9701dc3dd16d3d2a3b240
Charles COPART <charles.copart@hotmail.fr>
2021-01-10 20:21:13 +0100

    feat(UnitTestExercice2): add unit testing for exercise 2

---
+++ Exercice2/UnitTestExercice2/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercice2;
+
+namespace UnitTestExercice2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
